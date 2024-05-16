997394cc34a151ec460f78b075e9021ffb3f1283
Vinhf <thanhvinh1662003@gmail.com>
2024-05-16 10:26:55 +0700

    done 1/2

---
+++ Booking-backend/src/test/java/net/Bookingbackend/BookingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.Bookingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
